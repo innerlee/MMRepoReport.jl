8b11dacab69d82f334abf59f7b0d09c8df9aa3c7
lizz <lizz@sensetime.com>
2021-06-28 02:00:09 +0800

    haha

--- collect_location.jl
+++ collect_location.jl
@@ -34,53 +34,54 @@ repo = "mmclassification"
 repo = "mmaction2"
 repo = "mmdetection"
 repo = "mmcv"
+repo = "mm"
 
-df = CSV.File("data/$repo-stars.csv", delim = '\1') |> DataFrame
-locations = strip.(replace.(filter(!ismissing, df.location), ["@" => ""]))
+# df = CSV.File("data/$repo-stars.csv", delim = '\1') |> DataFrame
+# locations = strip.(replace.(filter(!ismissing, df.location), ["@" => ""]))
 
-const TOKEN = strip(read("pat2", String))
+# const TOKEN = strip(read("pat2", String))
 
-function execute_loc(query, token = TOKEN)
-    body = JSON3.write(Dict("location" => query, "options" => Dict("thumbMaps"=> false, "maxResults"=> 1)))
-    res = nothing
-    for i in 1:10
-        try
-            res = HTTP.request("POST", "http://www.mapquestapi.com/geocoding/v1/address?key=$token", [],body)
-        catch e
-            println("try $i time for $query")
-            sleep(0.2)
-        end
-        isnothing(res) || break
-    end
+# function execute_loc(query, token = TOKEN)
+#     body = JSON3.write(Dict("location" => query, "options" => Dict("thumbMaps"=> false, "maxResults"=> 1)))
+#     res = nothing
+#     for i in 1:10
+#         try
+#             res = HTTP.request("POST", "http://www.mapquestapi.com/geocoding/v1/address?key=$token", [],body)
+#         catch e
+#             println("try $i time for $query")
+#             sleep(0.2)
+#         end
+#         isnothing(res) || break
+#     end
 
-    try
-        loc = JSON3.read(String(res.body)).results[1].locations[1]
-        return (;country=loc.adminArea1, state=loc.adminArea3, city=loc.adminArea5, lat=loc.displayLatLng.lat, lng=loc.displayLatLng.lng)
-    catch e
-        loc = nothing
-        print(e)
-        return (;country="", state="", city="", lat=0, lng=0)
-    end
-# country, state, city
-end
+#     try
+#         loc = JSON3.read(String(res.body)).results[1].locations[1]
+#         return (;country=loc.adminArea1, state=loc.adminArea3, city=loc.adminArea5, lat=loc.displayLatLng.lat, lng=loc.displayLatLng.lng)
+#     catch e
+#         loc = nothing
+#         print(e)
+#         return (;country="", state="", city="", lat=0, lng=0)
+#     end
+# # country, state, city
+# end
 
-struct LOCATION
-    country::String
-    state::String
-    city::String
-    lat::Float64
-    lng::Float64
-end
+# struct LOCATION
+#     country::String
+#     state::String
+#     city::String
+#     lat::Float64
+#     lng::Float64
+# end
 
 
-result = @showprogress map(locations) do location
-    loc = execute_loc(location)
-    return LOCATION(loc.country, loc.state, loc.city, loc.lat, loc.lng)
-end
+# result = @showprogress map(locations) do location
+#     loc = execute_loc(location)
+#     return LOCATION(loc.country, loc.state, loc.city, loc.lat, loc.lng)
+# end
 
-df2 = DataFrame(result)
-df2 = df2[(!=)("").(df2.country), :]
-CSV.write("data/$repo-stars-locations.csv", result; delim = '\1', append = false)
+# df2 = DataFrame(result)
+# df2 = df2[(!=)("").(df2.country), :]
+# CSV.write("data/$repo-stars-locations.csv", result; delim = '\1', append = false)
 
 
 df2 = CSV.File("data/$repo-stars-locations.csv", delim = '\1') |> DataFrame
@@ -91,8 +92,9 @@ for r in eachrow(df2)
     folium.Circle(
        location=(r.lat, r.lng),
        popup=r.country * (ismissing(r.state) ? "" : ", $(r.state)") * (ismissing(r.city) ? "" : ", $(r.city)"),
-       radius=nums[(r.lat, r.lng)]*5000,
+       radius=min(100, nums[(r.lat, r.lng)])*1000,
        color="crimson",
+       opacity=0.25,
        fill=true,
        fill_color="crimson"
     ).add_to(m)
